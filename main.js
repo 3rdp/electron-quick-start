8dbe8f7abdb49e7e041e40974956922cd57324be
3rdp <third.pancake16@gmail.com>
2017-11-28 00:33:43 +0200

    Updated Readme for ShoppingList

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+const {Menu, ipcMain} = electron
 
 const path = require('path')
 const url = require('url')
@@ -10,6 +11,7 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let addWindow
 
 function createWindow () {
   // Create the browser window.
@@ -22,12 +24,77 @@ function createWindow () {
     slashes: true
   }))
 
+  mainWindow.on("close", function() {
+    app.quit()
+  })
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  Menu.setApplicationMenu(mainMenu)
+
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // if (process.env.NODE_ENV != "production") mainWindow.webContents.openDevTools()
 }
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({width: 300, height: 200})
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  
+  // Garbage collection handle
+  addWindow.on("close", function() {
+    addWindow = null;
+  })
+
+  ipcMain.on("item.add", function(e, item) {
+    console.log(item)
+    mainWindow.webContents.send("item.add", item)
+    addWindow.close()
+  })
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  Menu.setApplicationMenu(mainMenu)
+
+}
+
+const mainMenuTemplate = [
+  {
+    label: "File", 
+    submenu: [
+      {
+        label: "Add Item", 
+        click(){
+          createAddWindow()
+        }
+      }, {
+        label: "Clear Items", 
+        click(){
+          mainWindow.webContents.send("item.clear")
+        }
+      }, {
+        label: "Quit", 
+        accelerator: "Ctrl+Q",
+        click(){
+          app.quit()
+        }
+      }]
+  }]
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
+if (process.env.NODE_ENV != "production") {
+  mainMenuTemplate.push({
+    label: "DevTools", 
+    accelerator: "F12",
+    click(item, focusedWindow){
+      focusedWindow.toggleDevTools()
+    }
+  }, {
+    accelerator: "F5",
+    role: "reload"
+  })
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+let h1 = document.querySelector("h1")
+
+h1.style.color = 'red'
